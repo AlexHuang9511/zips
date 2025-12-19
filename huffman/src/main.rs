2058beced72ca2a68ca4759847e44e94f62896c3
Alex <alexanderw9511@gmail.com>
2025-12-19 03:48:20 -0800

    huffman - starting

--- huffman/src/main.rs
+++ huffman/src/main.rs
@@ -1,3 +1,26 @@
+use std::{collections::HashMap, env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    if args.len() != 2 {
+        println!("Usage: huf {{file}}");
+        return ();
+    }
+
+    let data: Vec<u8> = fs::read(&args[1]).expect("error opening file");
+    println!("{:?}", data);
+
+    let mut dict: HashMap<u8, u32> = HashMap::new();
+
+    for x in data {
+        match dict.get(&x) {
+            Some(&i) => dict.insert(x, i + 1),
+            _ => dict.insert(x, 1),
+        };
+    }
+
+    println!("Dict: {:?}", dict);
+
+    return ();
 }
