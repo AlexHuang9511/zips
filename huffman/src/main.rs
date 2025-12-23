a94862bf2e9498cee9b447c4156226602c6b4b78
Alex <alexanderw9511@gmail.com>
2025-12-22 20:30:46 -0800

    huffman - updated gitignore, finish encoding

--- huffman/src/main.rs
+++ huffman/src/main.rs
@@ -20,7 +20,7 @@ impl Node {
 }
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.freq.cmp(&other.freq)
+        self.freq.cmp(&other.freq).then(self.byte.cmp(&other.byte))
     }
 }
 impl PartialOrd for Node {
@@ -35,30 +35,51 @@ impl PartialEq for Node {
 }
 impl Eq for Node {}
 
-fn buildTree(tree: Node, freq: &HashMap<u8, u32>) {
-    let mut v: Vec<Node> = Vec::new();
+fn buildTree(tree: &mut Vec<Node>, freq: &HashMap<u8, u32>) {
     for (x, f) in freq {
-        v.push(Node {
+        tree.push(Node {
             freq: *f,
             byte: Some(*x),
             left: None,
             right: None,
         });
     }
-    v.sort();
+    tree.sort();
+    println!("list: {:?}\n", tree);
 
-    while v.len() > 1 {
-        let l = v.remove(0).clone();
-        let r = v.remove(0).clone();
-        v.push(Node {
+    while tree.len() > 1 {
+        let l = tree.remove(0).clone();
+        let r = tree.remove(0).clone();
+        tree.push(Node {
             freq: l.freq + r.freq,
             byte: None,
             left: Some(Box::new(l)),
             right: Some(Box::new(r)),
         });
-        v.sort();
+        tree.sort();
     }
-    println!("list: {:?}", v);
+    println!("list: {:?}", tree);
+}
+
+fn buildCodes(node: Node, prefix: String, codebook: &mut HashMap<u8, String>) {
+    match node.byte {
+        Some(b) => {
+            codebook.insert(b, prefix);
+            return ();
+        }
+        None => {
+            let left = prefix.clone() + "0";
+            let right = prefix.clone() + "1";
+            match node.left {
+                Some(n) => buildCodes(*n, left, codebook),
+                None => (),
+            }
+            match node.right {
+                Some(n) => buildCodes(*n, right, codebook),
+                None => (),
+            }
+        }
+    };
 }
 
 fn main() {
@@ -68,13 +89,21 @@ fn main() {
         println!("Usage: huf {{file}}");
         return ();
     }
+    let fname = args[1].clone();
+    let fComp: Vec<&str> = fname.split('.').collect();
+
+    if *fComp.last().unwrap() == "huf" {
+        println!("decode");
+    } else {
+        println!("encode");
+    }
 
     let data: Vec<u8> = fs::read(&args[1]).expect("error opening file");
     println!("{:?}", data);
 
     let mut freq: HashMap<u8, u32> = HashMap::new();
 
-    for x in data {
+    for x in data.clone() {
         match freq.get(&x) {
             Some(&i) => freq.insert(x, i + 1),
             _ => freq.insert(x, 1),
@@ -82,8 +111,23 @@ fn main() {
     }
 
     println!("Freq: {:?}", freq);
-    let mut tree: Node = Node::new();
-    buildTree(tree, &freq);
+    let mut tree: Vec<Node> = Vec::new();
+    buildTree(&mut tree, &freq);
+
+    println!("\ntree: {:?}", tree);
+    let mut codebook: HashMap<u8, String> = HashMap::new();
+    let mut prefix: String = "".to_string();
+    buildCodes(tree[0].clone(), prefix, &mut codebook);
+
+    println!("\ncodebook: {:?}", codebook);
+
+    let mut msg: Vec<u8> = Vec::new();
+    for byte in data.clone() {
+        msg.push(u8::from_str_radix(codebook.get(&byte).unwrap(), 2).unwrap());
+    }
+    println!("msg: {:?}", msg);
+
+    fs::write(fComp[0].clone().to_owned() + ".huf", msg);
 
     return ();
 }
